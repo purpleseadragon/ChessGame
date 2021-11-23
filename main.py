260668b1d06202743537235d855a7a6b5da64f20
purpleseadragon <oscardavine@gmail.com>
2021-11-24 01:30:21 +1000

    rook class update

--- main.py
+++ main.py
@@ -2,21 +2,24 @@
 import numpy as np
 import pieces
 
-board = [[' ', ' ', ' ', 'bk ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', 'wp', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', 'K', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', 'P', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+board = [['  ', '  ', '  ', '  ', 'bp', '  ', '  ', '  '],
+         ['  ', '  ', '  ', '  ', 'wp', '  ', '  ', '  '],
+         ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+         ['  ', 'wp', 'wp', '  ', '  ', '  ', 'bp', 'wh'],
+         ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+         ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+         ['  ', '  ', '  ', '  ', 'bp', '  ', '  ', '  '],
+         ['  ', '  ', '  ', '  ', 'wp', '  ', '  ', '  ']
          ]
 
 if __name__ == '__main__':
-    sample = pieces.Piece('white', (4, 5))
+    sample = pieces.Piece('white', (4, 5), board)
     print(sample.location)
     print(sample.colour)
-    print(sample.total_locat())
-    sample_rook = pieces.Rook('black', (2, 3))
-    print(np.array(sample_rook.possible_moves(board)))
-    print('maybe')
+    sample_rook = pieces.Rook('white', (3, 4), board)
+    print(np.array(sample_rook.possible_moves()))
+    print(sample_rook.move_piece((3, 6)))
+    print(sample_rook.location)
+    print(sample_rook.move_piece((6, 6)))
+    print(sample_rook.location)
+    print(sample_rook.print_board())
--- pieces.py
+++ pieces.py
@@ -1,27 +1,29 @@
 """File that contains piece classes"""
+import numpy as np
 
 class Piece:
-    def __init__(self, colour, location):
+    def __init__(self, colour, location, board):
         """Create a piece object with location information"""
         self.colour = colour
         self.location = location
+        self.board = board
         self._has_moved = False
         self._colour_letter = self.colour[0]
 
-    def total_locat(self):
-        """Test method"""
-        return self.location[0] * self.location[1]
 
-    def move(self, board):
+    def move_piece(self, board, move):
         pass
 
     def letter(self):
         """returns letter that represents that piece, i.e N for knight"""
         return 'yo no lo'
 
+    def print_board(self):
+        return np.array(self.board)
+
 
 class Rook(Piece):
-    def possible_moves(self, board):
+    def possible_moves(self):
         """returns list of possible moves for the given board state"""
         # board always square
         # rook moves up-down, left-right
@@ -31,33 +33,66 @@ class Rook(Piece):
         possible_vertical = []
         row = self.location[0]
         column = self.location[1]
-        # for num in range(len(board)):
-        #     if num != column:
-        #         possible_horizontal.append((row, num))
-        #     if num != row:
-        #         possible_vertical.append((num, column))
-        lower_vertical_list = list(range(row))
-        lower_vertical_list.reverse()
-
-        for num in lower_vertical_list:
-            if board[num][column] == ' ':
-                possible_vertical.append((num, column))
-            elif board[num][column][0] != self._colour_letter:
-                possible_vertical.append((num, column))
-                break
-            else:
-                break
-
-        sample_board = board.copy()
-
-        possible = possible_vertical + possible_horizontal
-        for coord in possible:
-            sample_board[coord[0]][coord[1]] = 'x'
-        sample_board[row][column] = 'R'
-
-        # check if any piece in the way
-
-        return sample_board
-    
 
 
+        lower_vertical_range = list(range(row))
+        lower_vertical_range.reverse()
+        upper_vertical_range = list(range(len(self.board)-row-1))
+        for x, num in enumerate(upper_vertical_range):
+            upper_vertical_range[x] += row + 1
+
+        possible_vertical += rook_move_helper_column(lower_vertical_range, self._colour_letter, self.board, column)
+        possible_vertical += rook_move_helper_column(upper_vertical_range, self._colour_letter, self.board, column)
+
+        lower_horizontal_range = list(range(column))
+        lower_horizontal_range.reverse()
+        upper_horizontal_range = list(range(len(self.board)-column-1))
+        for x, num in enumerate(upper_horizontal_range):
+            upper_horizontal_range[x] += column + 1
+
+        possible_horizontal += rook_move_helper_row(lower_horizontal_range, self._colour_letter, self.board, row)
+        possible_horizontal += rook_move_helper_row(upper_horizontal_range, self._colour_letter, self.board, row)
+
+        possible = set(possible_vertical + possible_horizontal)
+        # sample_board = board.copy()
+        #
+        # for coord in possible:
+        #     sample_board[coord[0]][coord[1]] = 'x '
+        # sample_board[row][column] = 'R '
+        return possible
+
+    def move_piece(self, move):
+        if move in self.possible_moves():
+            self.board[self.location[0]][self.location[1]] = '  '
+            self.location = move
+            self.board[self.location[0]][self.location[1]] = 'gp'  # gp is placeholder name
+        else:
+            return 'Pick a valid move pls'
+
+
+def rook_move_helper_column(moves_range, colour_letter, board, column):
+    # helps with creating possible moves
+    possible_moves_list = []
+    for num in moves_range:
+        if board[num][column] == '  ':
+            possible_moves_list.append((num, column))
+        elif board[num][column][0] != colour_letter:
+            possible_moves_list.append((num, column))
+            break
+        else:
+            break
+    return possible_moves_list
+
+
+def rook_move_helper_row(moves_range, colour_letter, board, row):
+    # helps with creating possible moves
+    possible_moves_list = []
+    for num in moves_range:
+        if board[row][num] == '  ':
+            possible_moves_list.append((row, num))
+        elif board[row][num][0] != colour_letter:
+            possible_moves_list.append((row, num))
+            break
+        else:
+            break
+    return possible_moves_list
